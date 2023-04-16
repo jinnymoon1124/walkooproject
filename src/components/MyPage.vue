7d23044489e0aeeb64a668829f84f08866b21363
Taeyeon Park <106387717+CherryBlossom99@users.noreply.github.com>
2023-04-16 18:42:00 +0900

    2023 04 16 18:41

--- src/components/MyPage.vue
+++ src/components/MyPage.vue
@@ -1,15 +1,19 @@
 <template>
-  <div class="mypage">
-    <div class="top">
-      <h1>마이페이지</h1>
-    </div>
-
-    <div class="information"></div>
-    <div class="namebox"></div>
-    <p class="name">센치누나애오</p>
-    <img src="../assets/girl.png" alt="사람" class="사용자캐릭터" />
-    <img src="../assets/알람버튼.png" alt="알람버튼" class="알람버튼" />
+  <div class="top">
+    <p class="top_name">마이페이지</p>
   </div>
+  <div class="middle">
+    <div class="user_name_box">
+      <p class="user_name">나는 미x노미양</p>
+    </div>
+    <div class="user_character_box">
+      <img src="../assets/user.png" alt="사용자캐릭터" class="user_character" />
+    </div>
+    <div class="alarm_box">
+      <img src="../assets/icon_alarm.png" alt="알람 아이콘"/>
+    </div>
+  </div>
+  <div class="bottom">홈 바</div>
 </template>
 
 <script>
@@ -19,77 +23,108 @@ export default {
 </script>
 
 <style>
-.mypage {
-  width: inherit;
-  height: 1623px;
-  position: relative;
-}
 .top {
-  background-color: #687089;
-  color: white;
-  width: inherit;
-  height: 185px;
+  position: relative;
+  width: 100%;
+  height: 12%;
+  display: flex;
+  justify-content: center; /* 가로 중앙에 위치 */
+  align-items: flex-start; /* 세로 위에서 70% 지점에 위치 */
+  background: #687089;
 }
-.top h1 {
+.top_name {
   position: absolute;
-  top: 80px;
-  left: 50%;
-  transform: translateX(-50%);
+  top: 70%; /* 세로로 위에서 70% 지점에 위치 */
+  transform: translateY(
+    -50%
+  ); /* 텍스트 박스의 높이의 절반만큼 위로 이동하여 중앙 정렬 */
+  width: 100%; /* 텍스트 박스의 너비를 100%로 설정하여 가로 중앙에 위치 */
+  text-align: center; /* 텍스트를 가운데 정렬 */
+
+  font-family: "Inter";
+  font-style: normal;
+  font-weight: 700;
+  font-size: 20px;
+  line-height: 21px;
+  /* identical to box height, or 105% */
+
+  text-align: center;
+  letter-spacing: -0.32px;
+
+  color: #ffffff;
 }
-.namebox {
+.middle {
+  position: relative;
+  width: 100%;
+  height: 80%;
+  display: flex;
+  justify-content: center; /* 가로 중앙에 위치 */
+  align-items: flex-start; /* 세로 위에서 70% 지점에 위치 */
+}
+.user_name_box {
   position: absolute;
-  width: 411px;
-  height: 164px;
-  left: 185px;
-  top: 249px;
+  width: 54%;
+  height: 10%;
+  top: 5%;
 
   background: #ffffff;
   box-shadow: 0px 4px 5px rgba(0, 0, 0, 0.25);
   border-radius: 10px;
 }
-.name {
+.user_name {
   position: absolute;
-  width: 231px;
-  height: 40px;
-  left: 267px;
-  top: 311px;
+  top: 50%; /* 세로로 위에서 70% 지점에 위치 */
+  transform: translateY(
+    -50%
+  ); /* 텍스트 박스의 높이의 절반만큼 위로 이동하여 중앙 정렬 */
+  width: 100%; /* 텍스트 박스의 너비를 100%로 설정하여 가로 중앙에 위치 */
 
   font-family: "Inter";
   font-style: normal;
   font-weight: 700;
   font-size: 22px;
   line-height: 21px;
-  /* or 95% */
+  /* identical to box height, or 95% */
 
   text-align: center;
   letter-spacing: -0.32px;
 
   color: #747474;
 }
-.information {
+.user_character_box {
   position: absolute;
-  width: 575px;
-  height: 307px;
-  left: 131px;
-  top: 332px;
+  width: 90px;
+  height: 90px;
+  top: 2%;
+  left: 3%;
+}
+.user_character {
+  width: 100%;
+  height: 100%;
+}
+.alarm_box {
+  position: absolute;
+  width: 64px;
+  height: 66px;
+  right: 2%;
+  top: 2%;
 
-  background: #eaeddf;
-  box-shadow: 0px 4px 5px rgba(0, 0, 0, 0.25);
-  border-radius: 10px;
+  display: flex;
+  justify-content: center; /* 수평 가운데 정렬 */
+  align-items: center; /* 수직 가운데 정렬 */
+
+  border-radius: 100%;
+  border: 3px solid #423a35;
+  background: #f6f6f6;
 }
-.사용자캐릭터 {
-  position: absolute;
-  width: 173px;
-  height: 189px;
-  left: 46px;
-  top: 224px;
-}
-.알람버튼 {
-  position: absolute;
-  width: 123.08px;
-  height: 126.65px;
-  left: 615.38px;
-  top: 195.73px;
+.bottom {
+  position: relative;
+  width: 100%;
+  height: 8%;
+
+  background: rgba(249, 249, 249, 0.94);
+  box-shadow: 0px -0.5px 0px rgba(0, 0, 0, 0.3);
+  backdrop-filter: blur(10px);
 }
 </style>
 
--- src/router.js
+++ src/router.js
@@ -1,18 +1,7 @@
 import { createWebHistory, createRouter } from 'vue-router'
-import MainPage from './components/MainPage.vue'
-import MyPage from './components/MyPage.vue'
 
 const routes = [
-    {
-        path: '/mainpage',
-        name: 'mainpage',
-        component: MainPage,
-    },
-    {
-        path: '/mypage',
-        name: 'mypage',
-        component: MyPage,
-    },
+
 ]
 
 const router = createRouter({
