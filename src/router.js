ff46ac2096ef677d303699b8841451d2f77a488a
cmkoi1 <cmkoi1003@gmail.com>
2023-04-19 22:03:01 +0900

    rank

--- src/router.js
+++ src/router.js
@@ -1,12 +1,36 @@
 import { createWebHistory, createRouter } from 'vue-router'
 import MainPage from './components/MainPage.vue'
+import Rank from './components/Rank.vue'
+import FamilyRank from './components/FamilyRank.vue'
+import FriendRank from './components/FriendRank.vue'
+import PetRank from './components/PetRank.vue'
 
 const routes = [
     {
         path: '/mainpage',
         name: 'mainpage',
         component: MainPage
-    }
+    },
+    {
+        path: '/rank',
+        name: 'rankpage',
+        component: Rank
+    },
+    {
+        path: '/rank/family',
+        name: 'familyrankpage',
+        component: FamilyRank
+    },
+    {
+        path: '/rank/friend',
+        name: 'frinedrankpage',
+        component: FriendRank
+    },
+    {
+        path: '/rank/pet',
+        name: 'petrankpage',
+        component: PetRank
+    },
 ]
 
 const router = createRouter({
